0148c648c74d75f41e4a9e3ac1576792f89efdd3
Dave Cohen <dcohen@infinio.com>
2015-02-11 16:14:19 -0500

    Binding html elements

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,8 +1,9 @@
 var phonecatApp = angular.module('chardbApp', []);
 
-phonecatApp.controller('HelloController', function ($scope, $http) {
+phonecatApp.controller('HelloController', function ($scope, $http, $sce) {
     $http.get('/character/54dbc0e4646174009a000000').
         success(function(data) {
-            console.log(data);
+            $scope.char = data;
+            $scope.char.description = $sce.trustAsHtml($scope.char.description);
         });
 });
