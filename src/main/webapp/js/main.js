6388169554520ab485be145de0495a4523918628
Dave Cohen <dcohen@infinio.com>
2015-02-11 16:27:34 -0500

    Editor addition

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,9 +1,13 @@
-var phonecatApp = angular.module('chardbApp', []);
+var chardbApp = angular.module('chardbApp', ['textAngular']);
 
-phonecatApp.controller('HelloController', function ($scope, $http, $sce) {
+chardbApp.controller('CharacterController', function ($scope, $http, $sce) {
     $http.get('/character/54dbc0e4646174009a000000').
         success(function(data) {
             $scope.char = data;
             $scope.char.description = $sce.trustAsHtml($scope.char.description);
+            $scope.orightml = $scope.char.description;
+            $scope.htmlcontent = $scope.orightml;
+            $scope.disabled = false;
         });
 });
+
