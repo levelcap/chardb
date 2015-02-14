8429a1d4a635fd432533596fd80dc61a792e6c5a
Dave Cohen <dcohen@moontoast.com>
2015-02-13 20:51:50 -0500

    Adding titles

--- src/main/java/com/brave/chardb/controller/PageController.java
+++ src/main/java/com/brave/chardb/controller/PageController.java
@@ -1,15 +1,14 @@
 package com.brave.chardb.controller;
 
-import com.brave.chardb.model.Character;
-import com.brave.chardb.model.User;
-import com.brave.chardb.repository.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.brave.chardb.model.Character;
+import com.brave.chardb.model.User;
+import com.brave.chardb.repository.CharacterRepository;
 
 /**
  * Created by dcohen on 2/13/15.
@@ -28,6 +27,7 @@ public class PageController extends BaseController {
     public String index(@PathVariable("id") String id, Model model) {
         Character character = characterRepository.findOne(id);
         model.addAttribute("character", character);
+        model.addAttribute("title", "CharDB - " + character.getName());
         return "char";
     }
 
@@ -39,8 +39,11 @@ public class PageController extends BaseController {
             if (existingCharacter == null) {
                 return "index";
             } else if (!existingCharacter.getUserId().equals(currentUser.getId())) {
-
+            	model.addAttribute("character", existingCharacter);
+                model.addAttribute("title", "CharDB - " + existingCharacter.getName());
+                return "char";
             }
+            model.addAttribute("title", "CharDB - Editing " + existingCharacter.getName());
             return "edit";
         }
         return "login";
